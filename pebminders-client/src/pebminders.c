64a3d37f0668746e0537cf561a593cecb2dc5974
Josh Watzman <jwatzman@jwatzman.org>
2013-07-06 21:44:29 -0700

    Hello world!

--- pebminders-client/src/pebminders.c
+++ pebminders-client/src/pebminders.c
@@ -2,11 +2,12 @@
 #include "pebble_app.h"
 #include "pebble_fonts.h"
 
-#define MY_UUID {0xA4, 0x1B, 0xB0, 0xE2, 0xD2, 0x62, 0x4E, 0xA6, 0xAA, 0x30, 0xED, 0xBE, 0x01, 0xE3, 0x8A, 0x02}
-PBL_APP_INFO(MY_UUID, "Simplicity", "Pebble Technology", 3, 0 /* App version */, RESOURCE_ID_IMAGE_MENU_ICON, APP_INFO_WATCH_FACE);
+#define MY_UUID {0x40, 0x5A, 0x82, 0xF9, 0x7E, 0xF2, 0x43, 0x6F, 0xA3, 0x8B, 0x59, 0x35, 0x68, 0xD0, 0x0C, 0xD7}
+PBL_APP_INFO(MY_UUID, "PebMinders", "Josh Watzman", 3, 0 /* App version */, RESOURCE_ID_IMAGE_MENU_ICON, APP_INFO_WATCH_FACE);
 
 Window window;
 
+TextLayer text_pebminder_layer;
 TextLayer text_date_layer;
 TextLayer text_time_layer;
 
@@ -25,12 +26,20 @@ void line_layer_update_callback(Layer *me, GContext* ctx) {
 
 void handle_init(AppContextRef ctx) {
 
-  window_init(&window, "Simplicity");
+  window_init(&window, "PebMinders");
   window_stack_push(&window, true /* Animated */);
   window_set_background_color(&window, GColorBlack);
 
   resource_init_current_app(&APP_RESOURCES);
 
+  // TODO factor out this setup?
+  // TODO consts for the frame
+  text_layer_init(&text_pebminder_layer, window.layer.frame);
+  text_layer_set_text_color(&text_pebminder_layer, GColorWhite);
+  text_layer_set_background_color(&text_pebminder_layer, GColorClear);
+  layer_set_frame(&text_pebminder_layer.layer, GRect(1, 1, 144-1, 168-68));
+  text_layer_set_font(&text_pebminder_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  layer_add_child(&window.layer, &text_pebminder_layer.layer);
 
   text_layer_init(&text_date_layer, window.layer.frame);
   text_layer_set_text_color(&text_date_layer, GColorWhite);
@@ -52,6 +61,8 @@ void handle_init(AppContextRef ctx) {
   line_layer.update_proc = &line_layer_update_callback;
   layer_add_child(&window.layer, &line_layer);
 
+  static char hello_text[] = "• Hello world!";
+  text_layer_set_text(&text_pebminder_layer, hello_text);
 
   // TODO: Update display here to avoid blank display on launch?
 }
